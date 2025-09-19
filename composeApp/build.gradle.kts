1e520135e9447c4c84ac3a0dc4155bdcf0b48c86
Pike <56088716+PikeNote@users.noreply.github.com>
2025-09-19 10:24:52 -0400

    Enable Android R8 Optimization - R8 code shrinking and optimization enabled for Android

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -112,7 +112,11 @@ android {
     }
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt")
+            )
         }
     }
     compileOptions {
